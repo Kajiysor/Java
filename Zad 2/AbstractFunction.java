62bd60c12d9e4b839aa5795d67ff2be436b5df6b
Kajiysor <mrbarman55@gmail.com>
2021-10-24 21:14:39 +0200

    Zad. 2 mostly finished

--- Zad 2/AbstractFunction.java
+++ Zad 2/AbstractFunction.java
@@ -2,6 +2,7 @@ public abstract class AbstractFunction {
 
     public static double precision = 1.0e-5;
     public static double increment = 1.0e-4;
+    public static double epsilon = 1.0e-5;
     abstract double f(double x);
 
     double getRootBisection(double xmin, double xmax){
@@ -48,6 +49,26 @@ public abstract class AbstractFunction {
             area += (increment/2) * (f(x1+x1_dist) + f(x1 + x1_dist - increment));
         }
         return (area*mod);
+    }
+
+    double golden_ratio_min(double a, double b){
+        double k = (Math.sqrt(5)-1)/2;
+        double xL = b - k * (b - a);
+        double xR = a + k * (b - a);
+
+        while ( (b-a) > epsilon){
+            if (f(xL) < f(xR)){
+                b = xR;
+                xR = xL;
+                xL = b - k * (b - a);
+            }
+            else{
+                a = xL;
+                xL = xR;
+                xR = a + k * (b - a);
+            }
+        }
+        return f((a+b)/2);
     }
     
 }
--- Zad 2/MyFunction.java
+++ Zad 2/MyFunction.java
@@ -1,7 +1,7 @@
 public class MyFunction extends AbstractFunction {
     @Override
     double f(double x){
-        return Math.sin(x);
+        return (Math.sin(x));
     }
 
     public static void main(String[] args) {
@@ -9,7 +9,7 @@ public class MyFunction extends AbstractFunction {
         AbstractFunction.precision=1.0e-5;
         System.out.println(f.getRootBisection(1, 4));
         System.out.println(f.definite_integral(0, 2));
-
+        System.out.println(f.golden_ratio_min(Math.PI, 2*Math.PI));
         
     }
     

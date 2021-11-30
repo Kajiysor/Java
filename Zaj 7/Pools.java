4535d3a5c96a706c24cad9373fc02db66bb557fa
Kajiysor <mrbarman55@gmail.com>
2021-11-30 18:50:21 +0100

    Zaj 7

---
+++ Zaj 7/Pools.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Pools {
+    public static void main(String[] args) {
+        // ExecutorService es = Executors.newFixedThreadPool(5);
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        for (int i = 1; i < 10; i++){
+            final int id = i;
+            es.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(id);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+            System.out.println(i + " submitted");
+        }
+    }
+}
---
+++ Zaj 7/Watki.java
@@ -0,0 +1,66 @@
+public class Watki {
+
+    boolean flag = true;
+
+    public synchronized void changeFlag(){
+        this.flag = !this.flag;
+        if (this.flag){
+            System.out.println("Zwolnienie blokady");
+            notifyAll(); // budzi wszystkie watki
+            // notify(); // budzi jeden watek
+        }
+    }
+
+    public synchronized void print(String s){ // Synchronized korzysta z blokady obiektu
+        while (!this.flag){
+            try {
+                System.out.println("Czekam zeby wypisac: " + s);
+                wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        changeFlag();
+        for (int i = 0; i < 10; i++){
+            System.out.println(i + " " + s);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final Watki w1 = new Watki(); // w2 = new Watki();
+        Thread t1 = new Thread(new Runnable(){
+            public void run(){
+                for (int i = 0; i < 10; i++){
+                    w1.print("(" + i + ")" + " Pierwszy watek"); // w1 -> jedna blokada 
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable(){
+            public void run(){
+                for (int i = 0; i < 10; i++){
+                    w1.print("(" + i + ")" + " Drugi watek"); // w2 -> druga blokada
+                }
+            }
+        });
+
+        Thread t3 = new Thread(new Runnable() {
+            public void run(){
+                for (int i = 0; i < 50; i++){
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    w1.changeFlag();
+                }
+            }
+        });
+        t1.start();
+        //t1.join();
+        t2.start();
+        t3.start();
+    }
+}
